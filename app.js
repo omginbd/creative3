78c48576349059b06d4494081797abf835df4a6b
Michael Collier <mikec@kuali.co>
2017-03-02 03:39:22 -0700

    Finish

--- app.js
+++ app.js
@@ -14,13 +14,15 @@ angular.module('StrangerThings', [])
   .directive('optionsmenu', optionsMenuDirective)
 
 function GameController($scope) {
+  $scope.win = false
+  $scope.lose = false
   $scope.textLog = []
   $scope.eleven = {
     stats: {
       hp: {
         name: 'Health',
-        max: 100,
-        cur: 100
+        max: 500,
+        cur: 500
       },
       mp: {
         name: 'Mana',
@@ -82,7 +84,7 @@ function GameController($scope) {
       }
     ]
   }
-  $scope.doAttack = _.partialRight(doAttack, $scope.eleven, $scope.demogorgan)
+  $scope.doAttack = _.partialRight(doAttack, $scope.eleven, $scope.demogorgan, $scope.textLog, $scope)
 }
 
 function statbarDirective() {
@@ -94,10 +96,7 @@ function statbarDirective() {
     replace: 'true',
     template: (
       `<div class="stat-bar">
-        {{stat.name}}:
-        <p>
-          {{stat.cur}} / {{stat.max}}
-        </p>
+        {{stat.name}}: {{stat.cur}} / {{stat.max}}
       </div>`
     )
   }
@@ -129,22 +128,89 @@ function optionsMenuDirective() {
   *
 ***/
 
-function doAttack(attackType, eleven, demogorgan) {
-  // Do magic math things here
-  // I suppose this could update the attack log as well, but that's your call
-  // Just return the updated stats, they'll be merged into the whole object
-  console.log(attackType)
-
-  return {
-    demogorgan: {
-      hp: 0,
-      mp: 0,
-      stamina: 0
-    },
-    eleven: {
-      hp: 0,
-      mp: 0,
-      stamina: 0
+function doAttack(attackType, eleven, demogorgan, textLog, $scope) {
+  switch (attackType) {
+    case ATTACK_TYPES.PUNCH:
+      if (eleven.stats.stamina.cur > 10) {
+        eleven.stats.stamina.cur = eleven.stats.stamina.cur - 10
+        demogorgan.stats.hp.cur = demogorgan.stats.hp.cur - eleven.attackDamage
+        textLog.unshift('Eleven used PUNCH for 10 damage!')
+    } else {
+        textLog.unshift('Not enough stamina! Use something else.')
+        return
     }
+      break
+    case ATTACK_TYPES.RAGE:
+      if (eleven.stats.mp.cur > 35) {
+        eleven.stats.mp.cur = eleven.stats.mp.cur - 35
+        demogorgan.stats.hp.cur = demogorgan.stats.hp.cur - 200
+        textLog.unshift('Eleven used RAGE for 200 damage!')
+      } else {
+        textLog.unshift('Not enough mana! Use something else.')
+        return
+      }
+      break
+    case ATTACK_TYPES.TRUCK:
+      if (eleven.stats.mp.cur > 20) {
+        eleven.stats.mp.cur = eleven.stats.mp.cur - 20
+        demogorgan.stats.hp.cur = demogorgan.stats.hp.cur - 100
+        textLog.unshift('Eleven used TRUCK for 100 damage!')
+      } else {
+        textLog.unshift('Not enough mana! Use something else.')
+        return
+      }
+      break
+    case ATTACK_TYPES.EGGO:
+      if (eleven.stats.waffles.cur > 1) {
+        eleven.stats.waffles.cur = eleven.stats.waffles.cur - 1
+        eleven.stats.hp.cur = eleven.stats.hp.cur + 100
+        textLog.unshift('Eleven ate an Eggo Waffle, healing for 100 health!')
+      } else {
+        textLog.unshift('No more waffles left!')
+        return
+      }
+      break
   }
+  if (demogorgan.stats.hp.cur <= 0) {
+    demogorgan.stats.hp.cur = 0
+    $scope.win = true
+    return;
+  }
+
+  eleven.stats.mp.cur = ((eleven.stats.mp.cur + 15) > eleven.stats.mp.max) ? eleven.stats.mp.max : eleven.stats.mp.cur + 15
+  eleven.stats.stamina.cur = ((eleven.stats.stamina.cur + 15) > eleven.stats.stamina.max) ? eleven.stats.stamina.max : eleven.stats.stamina.cur + 15
+  var attack = Math.floor(Math.random() * 100) % 3
+  switch (attack) {
+    case 0:
+      if (demogorgan.stats.stamina.cur > 200) {
+        demogorgan.stats.stamina.cur -= 200
+        eleven.stats.hp.cur -= 100
+        textLog.unshift('Demogorgan used CHARGE for 100 damage!')
+        break;
+      }
+    case 1:
+      if (demogorgan.stats.mp.cur > 200) {
+        demogorgan.stats.mp.cur -= 200
+        eleven.stats.hp.cur -= 150
+        textLog.unshift('Demogorgan cast HAUNT for 30 damage!')
+        break;
+      }
+    case 2:
+      if (demogorgan.stats.mp.cur > 300) {
+        demogorgan.stats.mp.cur -= 300
+        eleven.stats.hp.cur -= 100
+        textLog.unshift('Demogorgan cast CONSUME for 100 damage!')
+        break;
+      }
+    default:
+      textLog.unshift('Demogorgan is exhausted!')
+  }
+  demogorgan.stats.mp.cur = ((demogorgan.stats.mp.cur + 50) > demogorgan.stats.mp.max) ? demogorgan.stats.mp.max : demogorgan.stats.mp.cur + 50
+  demogorgan.stats.stamina.cur = ((demogorgan.stats.stamina.cur + 100) > demogorgan.stats.stamina.max) ? demogorgan.stats.stamina.max : demogorgan.stats.stamina.cur + 100
+  if (eleven.stats.hp.cur <= 0) {
+    eleven.stats.hp.cur = 0
+    $scope.lose = true
+    return;
+  }
+  return
 }
